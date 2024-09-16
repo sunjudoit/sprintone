45130ea0850e8230139ac9d115a501c4f754d306
sunjudoit <lovelysupergirl314@gmail.com>
2024-09-16 10:41:58 +0800

    Bubble Sort

--- P305532-C#-AT2-Sprint one/Astronomical Processing/Astronomical Processing/Form1.Designer.cs
+++ P305532-C#-AT2-Sprint one/Astronomical Processing/Astronomical Processing/Form1.Designer.cs
@@ -28,85 +28,77 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.button1 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
-            this.button3 = new System.Windows.Forms.Button();
-            this.listBox1 = new System.Windows.Forms.ListBox();
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.comboBox1 = new System.Windows.Forms.ComboBox();
-            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.ButtonEdit = new System.Windows.Forms.Button();
+            this.ButtonSearch = new System.Windows.Forms.Button();
+            this.ButtonSort = new System.Windows.Forms.Button();
+            this.ListBoxResults = new System.Windows.Forms.ListBox();
+            this.TextBoxInput = new System.Windows.Forms.TextBox();
+            this.ComboBoxHour = new System.Windows.Forms.ComboBox();
             this.SuspendLayout();
             // 
-            // button1
+            // ButtonEdit
             // 
-            this.button1.Location = new System.Drawing.Point(205, 112);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(213, 23);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
+            this.ButtonEdit.Location = new System.Drawing.Point(205, 112);
+            this.ButtonEdit.Name = "ButtonEdit";
+            this.ButtonEdit.Size = new System.Drawing.Size(213, 23);
+            this.ButtonEdit.TabIndex = 0;
+            this.ButtonEdit.Text = "SAVE/EDIT";
+            this.ButtonEdit.UseVisualStyleBackColor = true;
             // 
-            // button2
+            // ButtonSearch
             // 
-            this.button2.Location = new System.Drawing.Point(205, 155);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(213, 23);
-            this.button2.TabIndex = 1;
-            this.button2.Text = "button2";
-            this.button2.UseVisualStyleBackColor = true;
+            this.ButtonSearch.Location = new System.Drawing.Point(205, 155);
+            this.ButtonSearch.Name = "ButtonSearch";
+            this.ButtonSearch.Size = new System.Drawing.Size(213, 23);
+            this.ButtonSearch.TabIndex = 1;
+            this.ButtonSearch.Text = "SEARCH";
+            this.ButtonSearch.UseVisualStyleBackColor = true;
+            this.ButtonSearch.Click += new System.EventHandler(this.ButtonSearch_Click);
             // 
-            // button3
+            // ButtonSort
             // 
-            this.button3.Location = new System.Drawing.Point(205, 204);
-            this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(112, 23);
-            this.button3.TabIndex = 2;
-            this.button3.Text = "button3";
-            this.button3.UseVisualStyleBackColor = true;
+            this.ButtonSort.Location = new System.Drawing.Point(205, 204);
+            this.ButtonSort.Name = "ButtonSort";
+            this.ButtonSort.Size = new System.Drawing.Size(213, 23);
+            this.ButtonSort.TabIndex = 2;
+            this.ButtonSort.Text = "SEARCH ALL(SORT)";
+            this.ButtonSort.UseVisualStyleBackColor = true;
+            this.ButtonSort.Click += new System.EventHandler(this.ButtonSort_Click);
             // 
-            // listBox1
+            // ListBoxResults
             // 
-            this.listBox1.FormattingEnabled = true;
-            this.listBox1.Location = new System.Drawing.Point(40, 61);
-            this.listBox1.Name = "listBox1";
-            this.listBox1.Size = new System.Drawing.Size(120, 225);
-            this.listBox1.TabIndex = 3;
+            this.ListBoxResults.FormattingEnabled = true;
+            this.ListBoxResults.Location = new System.Drawing.Point(40, 61);
+            this.ListBoxResults.Name = "ListBoxResults";
+            this.ListBoxResults.Size = new System.Drawing.Size(120, 225);
+            this.ListBoxResults.TabIndex = 3;
             // 
-            // textBox1
+            // TextBoxInput
             // 
-            this.textBox1.Location = new System.Drawing.Point(205, 70);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(112, 20);
-            this.textBox1.TabIndex = 4;
+            this.TextBoxInput.Location = new System.Drawing.Point(205, 70);
+            this.TextBoxInput.Name = "TextBoxInput";
+            this.TextBoxInput.Size = new System.Drawing.Size(112, 20);
+            this.TextBoxInput.TabIndex = 4;
             // 
-            // comboBox1
+            // ComboBoxHour
             // 
-            this.comboBox1.FormattingEnabled = true;
-            this.comboBox1.Location = new System.Drawing.Point(323, 69);
-            this.comboBox1.Name = "comboBox1";
-            this.comboBox1.Size = new System.Drawing.Size(95, 21);
-            this.comboBox1.TabIndex = 5;
-            // 
-            // comboBox2
-            // 
-            this.comboBox2.FormattingEnabled = true;
-            this.comboBox2.Location = new System.Drawing.Point(323, 206);
-            this.comboBox2.Name = "comboBox2";
-            this.comboBox2.Size = new System.Drawing.Size(95, 21);
-            this.comboBox2.TabIndex = 6;
+            this.ComboBoxHour.FormattingEnabled = true;
+            this.ComboBoxHour.Location = new System.Drawing.Point(323, 69);
+            this.ComboBoxHour.Name = "ComboBoxHour";
+            this.ComboBoxHour.Size = new System.Drawing.Size(95, 21);
+            this.ComboBoxHour.TabIndex = 5;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(468, 343);
-            this.Controls.Add(this.comboBox2);
-            this.Controls.Add(this.comboBox1);
-            this.Controls.Add(this.textBox1);
-            this.Controls.Add(this.listBox1);
-            this.Controls.Add(this.button3);
-            this.Controls.Add(this.button2);
-            this.Controls.Add(this.button1);
+            this.Controls.Add(this.ComboBoxHour);
+            this.Controls.Add(this.TextBoxInput);
+            this.Controls.Add(this.ListBoxResults);
+            this.Controls.Add(this.ButtonSort);
+            this.Controls.Add(this.ButtonSearch);
+            this.Controls.Add(this.ButtonEdit);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
@@ -116,13 +108,12 @@
 
         #endregion
 
-        private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Button button2;
-        private System.Windows.Forms.Button button3;
-        private System.Windows.Forms.ListBox listBox1;
-        private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.ComboBox comboBox1;
-        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.Button ButtonEdit;
+        private System.Windows.Forms.Button ButtonSearch;
+        private System.Windows.Forms.Button ButtonSort;
+        private System.Windows.Forms.ListBox ListBoxResults;
+        private System.Windows.Forms.TextBox TextBoxInput;
+        private System.Windows.Forms.ComboBox ComboBoxHour;
     }
 }
 
--- P305532-C#-AT2-Sprint one/Astronomical Processing/Astronomical Processing/Form1.cs
+++ P305532-C#-AT2-Sprint one/Astronomical Processing/Astronomical Processing/Form1.cs
@@ -15,6 +15,86 @@ namespace Astronomical_Processing
         public Form1()
         {
             InitializeComponent();
+            FillArray();
+        }
+
+        static int hourMax = 24;
+        int[] hourArray = new int[hourMax];
+       
+
+        private void FillArray()
+        {
+            Random randData = new Random();
+            for (int i = 0; i < hourMax; i++)
+            {
+                // Random number
+                hourArray[i] = randData.Next(10,91);
+               // Console.WriteLine(hourArray[i]);
+            }
+        }      
+
+        private void ButtonSort_Click(object sender, EventArgs e)
+        {
+            BubbleSort();
+            ShowArray(0, 23);
+            MessageBox.Show("success!");
+        }
+
+        private void BubbleSort() 
+        {
+            int temp = 0;
+            for (int outer = 0; outer < hourMax ; outer++)
+            {
+                for (int inner = 0; inner < hourMax - 1; inner++) 
+                {
+                    if (hourArray[inner] > hourArray[inner + 1])
+                    {
+                        temp = hourArray[inner + 1];
+                        hourArray[inner + 1] = hourArray[inner];
+                        hourArray[inner] = temp;
+                    }
+                }
+            }
+            
+        }
+        private void ShowArray(int start, int end) 
+        {
+            ListBoxResults.Items.Clear();
+            for (int i = start; i < end; i++) 
+            {
+                ListBoxResults.Items.Add(hourArray[i]);
+            }
+        }
+
+        private void ButtonSearch_Click(object sender, EventArgs e)
+        {           
+            Binary_Search();
+        }
+        private void Binary_Search() 
+        {
+            int target;
+            int mid;
+            int startPoint = 0;
+            int endPoint = hourMax;
+            int.TryParse(TextBoxInput.Text, out target);
+            while (startPoint <= endPoint) 
+            {
+                mid = (startPoint + endPoint) /2;
+                if (hourArray[mid] == target)
+                {
+                    return;
+                }
+                else if (hourArray[mid] > target)
+                {
+                    endPoint = mid - 1;
+                }
+                else 
+                {
+                    startPoint = mid +1;
+                }
+            }
+            ShowArray(startPoint, endPoint);
+
         }
     }
 }
