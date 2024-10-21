66730bebff7dea0456e7cb0e8644412c0b1c9bf6
sunjudoit <lovelysupergirl314@gmail.com>
2024-10-21 15:14:02 +0800

    sprint two code
    sprint two code added

--- P305532-C#-AT2-Sprint one/Astronomical Processing/Astronomical Processing/Form1.Designer.cs
+++ P305532-C#-AT2-Sprint one/Astronomical Processing/Astronomical Processing/Form1.Designer.cs
@@ -28,58 +28,89 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             this.ButtonEdit = new System.Windows.Forms.Button();
             this.ButtonSearch = new System.Windows.Forms.Button();
             this.ButtonSort = new System.Windows.Forms.Button();
             this.ListBoxResults = new System.Windows.Forms.ListBox();
             this.TextBoxInput = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
+            this.ButtonSeqSearch = new System.Windows.Forms.Button();
+            this.ButtonMidExtreme = new System.Windows.Forms.Button();
+            this.ButtonMode = new System.Windows.Forms.Button();
+            this.ButtonAverage = new System.Windows.Forms.Button();
+            this.ButtonRange = new System.Windows.Forms.Button();
+            this.TextboxMidExtreme = new System.Windows.Forms.TextBox();
+            this.TextboxMode = new System.Windows.Forms.TextBox();
+            this.TextboxAverage = new System.Windows.Forms.TextBox();
+            this.TextboxRange = new System.Windows.Forms.TextBox();
+            this.ButtonReset = new System.Windows.Forms.Button();
+            this.toolTip1 = new System.Windows.Forms.ToolTip(this.components);
+            this.toolTip2 = new System.Windows.Forms.ToolTip(this.components);
+            this.toolTip3 = new System.Windows.Forms.ToolTip(this.components);
+            this.toolTip4 = new System.Windows.Forms.ToolTip(this.components);
+            this.toolTip5 = new System.Windows.Forms.ToolTip(this.components);
+            this.toolTip6 = new System.Windows.Forms.ToolTip(this.components);
+            this.toolTip7 = new System.Windows.Forms.ToolTip(this.components);
+            this.toolTip8 = new System.Windows.Forms.ToolTip(this.components);
+            this.toolTip9 = new System.Windows.Forms.ToolTip(this.components);
+            this.toolTip10 = new System.Windows.Forms.ToolTip(this.components);
+            this.toolTip11 = new System.Windows.Forms.ToolTip(this.components);
+            this.toolTip12 = new System.Windows.Forms.ToolTip(this.components);
+            this.toolTip13 = new System.Windows.Forms.ToolTip(this.components);
+            this.toolTip14 = new System.Windows.Forms.ToolTip(this.components);
+            this.toolTip15 = new System.Windows.Forms.ToolTip(this.components);
             this.SuspendLayout();
             // 
             // ButtonEdit
             // 
-            this.ButtonEdit.Location = new System.Drawing.Point(205, 112);
+            this.ButtonEdit.Location = new System.Drawing.Point(200, 174);
             this.ButtonEdit.Name = "ButtonEdit";
             this.ButtonEdit.Size = new System.Drawing.Size(213, 23);
             this.ButtonEdit.TabIndex = 0;
             this.ButtonEdit.Text = "SAVE/EDIT";
+            this.toolTip1.SetToolTip(this.ButtonEdit, "you can save or edit the data you want");
             this.ButtonEdit.UseVisualStyleBackColor = true;
             this.ButtonEdit.Click += new System.EventHandler(this.ButtonEdit_Click);
             // 
             // ButtonSearch
             // 
-            this.ButtonSearch.Location = new System.Drawing.Point(205, 155);
+            this.ButtonSearch.Location = new System.Drawing.Point(200, 86);
             this.ButtonSearch.Name = "ButtonSearch";
             this.ButtonSearch.Size = new System.Drawing.Size(213, 23);
             this.ButtonSearch.TabIndex = 1;
             this.ButtonSearch.Text = "SEARCH";
+            this.toolTip1.SetToolTip(this.ButtonSearch, "you can search the data using binary search");
             this.ButtonSearch.UseVisualStyleBackColor = true;
             this.ButtonSearch.Click += new System.EventHandler(this.ButtonSearch_Click);
             // 
             // ButtonSort
             // 
-            this.ButtonSort.Location = new System.Drawing.Point(205, 204);
+            this.ButtonSort.Location = new System.Drawing.Point(200, 145);
             this.ButtonSort.Name = "ButtonSort";
             this.ButtonSort.Size = new System.Drawing.Size(213, 23);
             this.ButtonSort.TabIndex = 2;
             this.ButtonSort.Text = "SORT";
+            this.toolTip1.SetToolTip(this.ButtonSort, "you can sort data in ascending order");
             this.ButtonSort.UseVisualStyleBackColor = true;
             this.ButtonSort.Click += new System.EventHandler(this.ButtonSort_Click);
             // 
             // ListBoxResults
             // 
             this.ListBoxResults.FormattingEnabled = true;
-            this.ListBoxResults.Location = new System.Drawing.Point(40, 61);
+            this.ListBoxResults.Location = new System.Drawing.Point(39, 46);
             this.ListBoxResults.Name = "ListBoxResults";
-            this.ListBoxResults.Size = new System.Drawing.Size(120, 225);
+            this.ListBoxResults.Size = new System.Drawing.Size(120, 238);
             this.ListBoxResults.TabIndex = 3;
+            this.toolTip1.SetToolTip(this.ListBoxResults, "It shows the entire data searched");
             // 
             // TextBoxInput
             // 
-            this.TextBoxInput.Location = new System.Drawing.Point(205, 70);
+            this.TextBoxInput.Location = new System.Drawing.Point(200, 48);
             this.TextBoxInput.Name = "TextBoxInput";
             this.TextBoxInput.Size = new System.Drawing.Size(213, 20);
             this.TextBoxInput.TabIndex = 4;
+            this.toolTip1.SetToolTip(this.TextBoxInput, "Insert a data you want to search or edit");
             // 
             // label1
             // 
@@ -90,11 +121,119 @@
             this.label1.TabIndex = 6;
             this.label1.Text = "Astronomical Processing";
             // 
+            // ButtonSeqSearch
+            // 
+            this.ButtonSeqSearch.Location = new System.Drawing.Point(200, 116);
+            this.ButtonSeqSearch.Name = "ButtonSeqSearch";
+            this.ButtonSeqSearch.Size = new System.Drawing.Size(213, 23);
+            this.ButtonSeqSearch.TabIndex = 7;
+            this.ButtonSeqSearch.Text = "SEQUENTIAL SEARCH";
+            this.toolTip1.SetToolTip(this.ButtonSeqSearch, "you can search the data using liner search");
+            this.ButtonSeqSearch.UseVisualStyleBackColor = true;
+            this.ButtonSeqSearch.Click += new System.EventHandler(this.ButtonSeqSearch_Click);
+            // 
+            // ButtonMidExtreme
+            // 
+            this.ButtonMidExtreme.Location = new System.Drawing.Point(200, 203);
+            this.ButtonMidExtreme.Name = "ButtonMidExtreme";
+            this.ButtonMidExtreme.Size = new System.Drawing.Size(109, 23);
+            this.ButtonMidExtreme.TabIndex = 8;
+            this.ButtonMidExtreme.Text = "MID-EXTREME";
+            this.toolTip1.SetToolTip(this.ButtonMidExtreme, "Calulate the intermediate valude between the smallest and largest numbers");
+            this.ButtonMidExtreme.UseVisualStyleBackColor = true;
+            this.ButtonMidExtreme.Click += new System.EventHandler(this.ButtonMidExtreme_Click);
+            // 
+            // ButtonMode
+            // 
+            this.ButtonMode.Location = new System.Drawing.Point(200, 232);
+            this.ButtonMode.Name = "ButtonMode";
+            this.ButtonMode.Size = new System.Drawing.Size(109, 23);
+            this.ButtonMode.TabIndex = 9;
+            this.ButtonMode.Text = "MODE";
+            this.toolTip1.SetToolTip(this.ButtonMode, "Claulate the value that appears most frequently in a data");
+            this.ButtonMode.UseVisualStyleBackColor = true;
+            this.ButtonMode.Click += new System.EventHandler(this.ButtonMode_Click);
+            // 
+            // ButtonAverage
+            // 
+            this.ButtonAverage.Location = new System.Drawing.Point(200, 261);
+            this.ButtonAverage.Name = "ButtonAverage";
+            this.ButtonAverage.Size = new System.Drawing.Size(109, 21);
+            this.ButtonAverage.TabIndex = 10;
+            this.ButtonAverage.Text = "AVERAGE";
+            this.toolTip1.SetToolTip(this.ButtonAverage, "Calculate value that best represents a set of data");
+            this.ButtonAverage.UseVisualStyleBackColor = true;
+            this.ButtonAverage.Click += new System.EventHandler(this.ButtonAverage_Click);
+            // 
+            // ButtonRange
+            // 
+            this.ButtonRange.Location = new System.Drawing.Point(200, 286);
+            this.ButtonRange.Name = "ButtonRange";
+            this.ButtonRange.Size = new System.Drawing.Size(109, 23);
+            this.ButtonRange.TabIndex = 11;
+            this.ButtonRange.Text = "RANGE";
+            this.toolTip1.SetToolTip(this.ButtonRange, "Calculate the difference between the lowest and highest values");
+            this.ButtonRange.UseVisualStyleBackColor = true;
+            this.ButtonRange.Click += new System.EventHandler(this.ButtonRange_Click);
+            // 
+            // TextboxMidExtreme
+            // 
+            this.TextboxMidExtreme.Location = new System.Drawing.Point(316, 205);
+            this.TextboxMidExtreme.Name = "TextboxMidExtreme";
+            this.TextboxMidExtreme.Size = new System.Drawing.Size(97, 20);
+            this.TextboxMidExtreme.TabIndex = 12;
+            this.toolTip1.SetToolTip(this.TextboxMidExtreme, "Diplyed Mid-Extreme value");
+            // 
+            // TextboxMode
+            // 
+            this.TextboxMode.Location = new System.Drawing.Point(316, 232);
+            this.TextboxMode.Name = "TextboxMode";
+            this.TextboxMode.Size = new System.Drawing.Size(97, 20);
+            this.TextboxMode.TabIndex = 13;
+            this.toolTip1.SetToolTip(this.TextboxMode, "Displyed Mode value");
+            // 
+            // TextboxAverage
+            // 
+            this.TextboxAverage.Location = new System.Drawing.Point(316, 261);
+            this.TextboxAverage.Name = "TextboxAverage";
+            this.TextboxAverage.Size = new System.Drawing.Size(97, 20);
+            this.TextboxAverage.TabIndex = 14;
+            this.toolTip1.SetToolTip(this.TextboxAverage, "Displalyed Average value");
+            // 
+            // TextboxRange
+            // 
+            this.TextboxRange.Location = new System.Drawing.Point(316, 289);
+            this.TextboxRange.Name = "TextboxRange";
+            this.TextboxRange.Size = new System.Drawing.Size(97, 20);
+            this.TextboxRange.TabIndex = 15;
+            this.toolTip1.SetToolTip(this.TextboxRange, "DIsplayed Range value");
+            // 
+            // ButtonReset
+            // 
+            this.ButtonReset.Location = new System.Drawing.Point(39, 287);
+            this.ButtonReset.Name = "ButtonReset";
+            this.ButtonReset.Size = new System.Drawing.Size(120, 23);
+            this.ButtonReset.TabIndex = 16;
+            this.ButtonReset.Text = "RESET";
+            this.toolTip1.SetToolTip(this.ButtonReset, "It will restart");
+            this.ButtonReset.UseVisualStyleBackColor = true;
+            this.ButtonReset.Click += new System.EventHandler(this.ButtonReset_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(468, 343);
+            this.Controls.Add(this.ButtonReset);
+            this.Controls.Add(this.TextboxRange);
+            this.Controls.Add(this.TextboxAverage);
+            this.Controls.Add(this.TextboxMode);
+            this.Controls.Add(this.TextboxMidExtreme);
+            this.Controls.Add(this.ButtonRange);
+            this.Controls.Add(this.ButtonAverage);
+            this.Controls.Add(this.ButtonMode);
+            this.Controls.Add(this.ButtonMidExtreme);
+            this.Controls.Add(this.ButtonSeqSearch);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.TextBoxInput);
             this.Controls.Add(this.ListBoxResults);
@@ -116,6 +255,31 @@
         private System.Windows.Forms.ListBox ListBoxResults;
         private System.Windows.Forms.TextBox TextBoxInput;
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button ButtonSeqSearch;
+        private System.Windows.Forms.Button ButtonMidExtreme;
+        private System.Windows.Forms.Button ButtonMode;
+        private System.Windows.Forms.Button ButtonAverage;
+        private System.Windows.Forms.Button ButtonRange;
+        private System.Windows.Forms.TextBox TextboxMidExtreme;
+        private System.Windows.Forms.TextBox TextboxMode;
+        private System.Windows.Forms.TextBox TextboxAverage;
+        private System.Windows.Forms.TextBox TextboxRange;
+        private System.Windows.Forms.Button ButtonReset;
+        private System.Windows.Forms.ToolTip toolTip1;
+        private System.Windows.Forms.ToolTip toolTip2;
+        private System.Windows.Forms.ToolTip toolTip3;
+        private System.Windows.Forms.ToolTip toolTip4;
+        private System.Windows.Forms.ToolTip toolTip5;
+        private System.Windows.Forms.ToolTip toolTip6;
+        private System.Windows.Forms.ToolTip toolTip7;
+        private System.Windows.Forms.ToolTip toolTip8;
+        private System.Windows.Forms.ToolTip toolTip9;
+        private System.Windows.Forms.ToolTip toolTip10;
+        private System.Windows.Forms.ToolTip toolTip11;
+        private System.Windows.Forms.ToolTip toolTip12;
+        private System.Windows.Forms.ToolTip toolTip13;
+        private System.Windows.Forms.ToolTip toolTip14;
+        private System.Windows.Forms.ToolTip toolTip15;
     }
 }
 
--- P305532-C#-AT2-Sprint one/Astronomical Processing/Astronomical Processing/Form1.cs
+++ P305532-C#-AT2-Sprint one/Astronomical Processing/Astronomical Processing/Form1.cs
@@ -8,6 +8,7 @@ using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
 
 // SUNJU CHOI, Sprint ONE
 // Date: 16.09.2024
@@ -25,7 +26,7 @@ namespace Astronomical_Processing
         // Array for 24 hours
         const int hourMax = 24;
         int[] hourArray = new int[hourMax];
-      
+
         // checking if the array is sorted
         bool isSorted = false;
 
@@ -34,6 +35,7 @@ namespace Astronomical_Processing
             InitializeComponent();
             FillArray();
             ShowArray();
+
         }
 
         /// <summary>
@@ -45,9 +47,9 @@ namespace Astronomical_Processing
             Random randData = new Random();
             for (int i = 0; i < hourMax; i++)
             {
-                
-                hourArray[i] = randData.Next(10,91);
-              
+
+                hourArray[i] = randData.Next(10, 91);
+
             }
         }
 
@@ -57,6 +59,7 @@ namespace Astronomical_Processing
         /// <param name=""> </param>
         private void ButtonSort_Click(object sender, EventArgs e)
         {
+        
             BubbleSort();
             ShowArray();
             MessageBox.Show("Data sorted successfully!");
@@ -66,36 +69,36 @@ namespace Astronomical_Processing
         ///  Bubble Sort Algorithm
         /// </summary>
         /// <param name=""> </param>
-        private void BubbleSort() 
+        private void BubbleSort()
         {
             int temp = 0;
-        
-            for (int outer = 0; outer < hourMax ; outer++)
+
+            for (int outer = 0; outer < hourMax; outer++)
             {
-                for (int inner = 0; inner < hourMax - 1; inner++) 
+                for (int inner = 0; inner < hourMax - 1; inner++)
                 {
                     if (hourArray[inner] > hourArray[inner + 1])
                     {
                         temp = hourArray[inner + 1];
                         hourArray[inner + 1] = hourArray[inner];
                         hourArray[inner] = temp;
-                     
+
                     }
                 }
             }
 
             isSorted = true;
-    }
+        }
 
         /// <summary>
         ///  Display data in the ListBox
         /// </summary>
         /// <param name=""> </param>
-        private void ShowArray() 
+        private void ShowArray()
         {
             ListBoxResults.Items.Clear();
             TextBoxInput.Clear();
-            for (int i = 0; i < hourMax; i++) 
+            for (int i = 0; i < hourMax; i++)
             {
                 ListBoxResults.Items.Add(hourArray[i]);
 
@@ -109,7 +112,8 @@ namespace Astronomical_Processing
         private void ButtonSearch_Click(object sender, EventArgs e)
         {
 
-           // ListBoxResults.Items.Clear();
+            TextboxClear();
+
             if (string.IsNullOrWhiteSpace(TextBoxInput.Text))
             {
                 MessageBox.Show("Please enter the data you want to search");
@@ -122,7 +126,7 @@ namespace Astronomical_Processing
                 {
                     MessageBox.Show("Please sort before you search");
                     return;
-                   
+
                 }
 
                 int resultSearch = BinarySearch(searchData);
@@ -142,7 +146,7 @@ namespace Astronomical_Processing
                 }
                 TextBoxInput.Clear();
             }
-            else 
+            else
             {
                 MessageBox.Show("Enter vaild data!");
                 return;
@@ -153,28 +157,28 @@ namespace Astronomical_Processing
         ///  Binary Search Algorithm
         /// </summary>
         /// <param name="target"> this is the value entered by user in the text box to search </param> 
-        private int BinarySearch(int target) 
+        private int BinarySearch(int target)
         {
             int mid;
             int startPoint = 0;
             int endPoint = hourMax - 1;
-            while (startPoint <= endPoint) 
+            while (startPoint <= endPoint)
             {
-                mid = (startPoint + endPoint) /2;
+                mid = (startPoint + endPoint) / 2;
                 if (hourArray[mid] == target)
                 {
                     ListBoxResults.Items.Clear();
-                    
+
                     return mid;
-                    
+
                 }
                 else if (hourArray[mid] > target)
                 {
                     endPoint = mid - 1;
                 }
-                else 
+                else
                 {
-                    startPoint = mid +1;
+                    startPoint = mid + 1;
                 }
             }
             return -1;
@@ -188,7 +192,9 @@ namespace Astronomical_Processing
         private void ButtonEdit_Click(object sender, EventArgs e)
         {
 
-            if (ListBoxResults.SelectedIndex == -1) 
+            TextboxClear();
+
+            if (ListBoxResults.SelectedIndex == -1)
             {
                 MessageBox.Show("Please select an data on the listbox to edit");
                 return;
@@ -198,8 +204,8 @@ namespace Astronomical_Processing
                 MessageBox.Show("Please enter the data you want to edit or save");
                 return;
             }
-           
-            if (int.TryParse(TextBoxInput.Text, out int newData)) 
+
+            if (int.TryParse(TextBoxInput.Text, out int newData))
             {
 
                 hourArray[ListBoxResults.SelectedIndex] = newData;
@@ -208,7 +214,7 @@ namespace Astronomical_Processing
                 isSorted = false;
 
                 MessageBox.Show("Data uptated successfully!");
-          
+
             }
             else
             {
@@ -217,6 +223,238 @@ namespace Astronomical_Processing
 
         }
 
+        /// <summary>
+        ///  Sequential Search Button Click
+        /// </summary>
+        /// <param name=""> </param> 
+        private void ButtonSeqSearch_Click(object sender, EventArgs e)
+        {
+            TextboxClear();
+
+
+            if (string.IsNullOrWhiteSpace(TextBoxInput.Text))
+            {
+                MessageBox.Show("Please enter the data you want to search");
+                return;
+            }
+
+            if (int.TryParse(TextBoxInput.Text, out int seqSearchData))
+            {
+
+                int resultSeqSearch = SequentialSearch(seqSearchData);
+
+
+                if (resultSeqSearch != -1)
+                {
+                    ShowArray();
+                    ListBoxResults.SelectedIndex = resultSeqSearch;
+                    MessageBox.Show("Success!");
+                }
+                else
+                {
+                    ShowArray();
+                    MessageBox.Show("not found!");
+                    return;
+                }
+                TextBoxInput.Clear();
+            }
+            else
+            {
+                MessageBox.Show("Enter vaild data!");
+                return;
+            }
+        }
+
+        private int SequentialSearch(int target)
+        {
+            for (int i = 0; i < hourMax; i++)
+            {
+                if (hourArray[i] == target)
+                {
+                    return i;
+                }
+
+            }
+            return -1;
+        }
+
+        /// <summary>
+        ///  MidExtreme Button Click
+        /// </summary>
+        /// <param name=""> </param>
+        private void ButtonMidExtreme_Click(object sender, EventArgs e)
+        {
+            double midExtreme = CalculatemidExtreme();
+            TextboxMidExtreme.Text = midExtreme.ToString();
+        }
+
+        /// <summary>
+        ///  Mode Button Click
+        /// </summary>
+        /// <param name=""> </param>
+        private void ButtonMode_Click(object sender, EventArgs e)
+        {
+            
+            int mode = CalculateMode();
+            if (mode != -1)
+            {
+                TextboxMode.Text = mode.ToString();
+            }
+            else 
+            {
+                MessageBox.Show("There is no mode!");
+            }
+        }
+
+        /// <summary>
+        ///  Average Button Click
+        /// </summary>
+        /// <param name=""> </param>
+        private void ButtonAverage_Click(object sender, EventArgs e)
+        {
+            double average = CalculateAverage();
+            TextboxAverage.Text = average.ToString();
+        }
+
+        /// <summary>
+        ///  Range Button Click
+        /// </summary>
+        /// <param name=""> </param>
+        private void ButtonRange_Click(object sender, EventArgs e)
+        {
+            int range = CalculateRange();
+            TextboxRange.Text = range.ToString();
+        }
+
+        /// <summary>
+        ///  Calculate Mid-Extreme
+        /// </summary>
+        /// <param name=""> </param>
+        private double CalculatemidExtreme()
+        {
+            int minData = hourArray[0];
+            int maxData = hourArray[0];
+
+            for (int i = 1; i < hourMax; i++)
+            {
+                if (hourArray[i] < minData)
+                {
+                    minData = hourArray[i];
+                }
+                if (hourArray[i] > maxData)
+                {
+                    maxData = hourArray[i];
+                }
+            }
+            return (minData + maxData) / 2.0;
+        }
+
+        /// <summary>
+        ///  Calculate Average
+        /// </summary>
+        /// <param name=""> </param>
+        private double CalculateAverage()
+        {
+            int sumAllData = 0;
+            for (int i = 0; i < hourMax; i++)
+            {
+                sumAllData = sumAllData + hourArray[i];
+            }
+            return sumAllData / 24;
+        }
+
+        /// <summary>
+        ///  Calculate Range
+        /// </summary>
+        /// <param name=""> </param>
+        private int CalculateRange()
+        {
+            int minData = hourArray[0];
+            int maxData = hourArray[0];
+
+            for (int i = 1; i < hourMax; i++)
+            {
+                if (hourArray[i] < minData)
+                {
+                    minData = hourArray[i];
+                }
+                if (hourArray[i] > maxData)
+                {
+                    maxData = hourArray[i];
+                }
+            }
+            return maxData - minData;
+        }
+
+        /// <summary>
+        ///  Calculate Mode
+        /// </summary>
+        /// <param name=""> </param>
+        private int CalculateMode()
+        {
+            int mode = hourArray[0];
+            int maxRepeat = 0;
+            int repeat = 0;
+            bool multiMode = false;
+
+            for (int i = 0; i < hourMax; i++) 
+            {
+                repeat = 0;
+
+                for (int j = 1; j < hourMax; j++)
+                {
+                    if (hourArray[i] == hourArray[j])
+                    {
+                        repeat = repeat + 1;
+                    }
+                }
+                if (repeat > maxRepeat)
+                {
+                    maxRepeat = repeat;
+                    mode = hourArray[i];
+                    multiMode = false; 
+
+                }
+                else if (repeat == maxRepeat & hourArray[i] != mode) 
+                {
+                    multiMode = true;
+                }
+            }
+            if (!multiMode)
+            {
+                return mode;
+            } 
+            return -1;
        
+        }
+
+        /// <summary>
+        ///  Reset button Click
+        /// </summary>
+        /// <param name=""> </param>
+        private void ButtonReset_Click(object sender, EventArgs e)
+        {
+            FillArray();
+            ShowArray();
+            TextboxMidExtreme.Clear();
+            TextboxMode.Clear();
+            TextboxAverage.Clear();
+            TextboxRange.Clear();
+
+        }
+
+        /// <summary>
+        ///  Textbox clear
+        /// </summary>
+        /// <param name=""> </param>
+        private void TextboxClear() 
+        {
+            TextboxMidExtreme.Clear();
+            TextboxMode.Clear();
+            TextboxAverage.Clear();
+            TextboxRange.Clear();
+        }
+
+
     }
 }
